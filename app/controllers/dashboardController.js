6e05258fc4a6edb2746e32bfec9aa8ffc0ae450b
rafa <Rafael Felipe>
2018-05-23 22:46:48 -0300

    Dashboard

--- app/controllers/dashboardController.js
+++ app/controllers/dashboardController.js
@@ -1,5 +1,18 @@
+const { Project, User } = require('../models');
+
 module.exports = {
-  index(req, res) {
-    return res.render('dashboard/index');
+  async index(req, res, next) {
+    try {
+      const usuario = await User.findById(req.session.user.id);
+
+      const projects = await Project.findAll({
+        where: {
+          UserId: req.session.user.id,
+        },
+      });
+      return res.render('dashboard/index', { projects, usuario });
+    } catch (err) {
+      return next(err);
+    }
   },
 };
